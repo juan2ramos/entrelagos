cc0adc0763442e2e8de070ae1b627df23bcc8dbe
juan2ramos <juan2ramos@gmail.com>
2015-06-23 09:21:09 -0500

    mail

--- email.php
+++ email.php
@@ -1,33 +1,34 @@
 <?php
-
 if (!empty($_POST)) {
-
-    if(empty($_POST['name-user']) || empty($_POST['company'])|| empty($_POST['phone']) || empty($_POST['dirigido']) || empty($_POST['comment'])){
+    if(empty($_POST['name-user']) || empty($_POST['email'])|| empty($_POST['phone']) || empty($_POST['comment']) ){
         $arrayMsj['success'] = FALSE;
         $arrayMsj['message'] = 'Todos los campos son requeridos';
         echo (json_encode($arrayMsj));
         exit;
     }
-    require '/home/redlat/public_html/via/include/PHPMailer/PHPMailerAutoload.php';
+    require '/home3/simonsei/mailer/PHPMailerAutoload.php';
     $mail = new PHPMailer;
-    $mail->IsSendmail();
-    $mail->FromName ='Formulario Escena 70' ;
+    $mail->FromName ='Formulario Entre Lagos' ;
     $mail->CharSet = 'UTF-8';
-    $mail->From = 'info@complejoteatral.gob.ar';
-    $mail->Subject = 'Mensaje de circulart Web';
+    $mail->From = 'webmaster@simonsein.com';
+    $mail->Subject = 'Mensaje Entre Lagos';
     $mail->MsgHTML('Mensaje con HTML');
-    $template = '<h1>Mensaje enviado desde el formulario de escena 70</h1><br><br>';
+    $template = '<h1>Mensaje enviado desde el formulario de Entre Lagos</h1><br><br>';
     $template .= 'Nombre: ' . $_POST['name-user'] . '<br>';
     $template .= 'Email: ' . $_POST['email'] .'<br>';
     $template .= 'telefono: ' . $_POST['phone'] .'<br>';
-    $template .= 'Compañia: ' . $_POST['company'] .'<br>';
-    $template .= 'Mensaje: <br>' . $_POST['comment'] ;
+    $template .= 'Comentario: ' . $_POST['comment'] .'<br>';
     $mail->Body = $template;
-    $mail->AddAddress($_POST['dirigido'], '');
-    $mail->Send();
+    $mail->AddAddress('webmaster@simonsein.com', '');
+    $mail->AddAddress('andres.rivera@simonsein.com ', '');
+    if($mail->Send()){
+        $arrayMsj['success'] = TRUE;
+        $arrayMsj['message'] = 'Felicitaciones, su mensaje a sido enviado con éxito!!';
+    }else{
+        $arrayMsj['success'] = false;
+        $arrayMsj['message'] = 'Su mensaje no a sido enviado con éxito!!';
+    }
 
-    $arrayMsj['success'] = TRUE;
-    $arrayMsj['message'] = 'Felicitaciones, su mensaje a sido enviado con éxito!!';
     echo (json_encode($arrayMsj));
     exit;
 }else{
